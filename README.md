6bbf68dd5bb2b8910cfffac55f17619efec24484
Simon Samuel <salviensky@gmail.com>
2023-09-26 00:07:53 +0100

    chore: analyzed running time of implementation docs: added documentation for source code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Sum-Pairs
+
+Assessment submission by Simon Samuel.
+
+## Setup
+
+```bash
+yarn
+```
+
+## Executing
+
+```bash
+yarn start
+```
+
+## Testing
+
+```bash
+yarn test
+```
--- src/findPairsWithSum.ts
+++ src/findPairsWithSum.ts
@@ -1,3 +1,6 @@
+/**
+ * A Simple Helper function for validating that two arrays are same.
+ * */
 function compareArray(arr1: number[], arr2: number[]): boolean {
   if (arr1.length != arr2.length) return false;
 
@@ -10,20 +13,38 @@ function compareArray(arr1: number[], arr2: number[]): boolean {
   return same;
 }
 
+/**
+ * Algorithm Analysis:
+ * The running time complexity of findPairsWithSum() is given by:
+ *                Ο(mlogm + (m - n)k)
+ *    where:
+ *          mlogm => Time required to sort the array in place.
+ *          m => Total number of elements in the array.
+ *          n => Total number of elements greater than the target value.
+ *          k => Current amount of elements in the pairs array that need to be
+ *          compared.
+ */
 export function findPairsWithSum(arr: number[], target: number) {
+  // Safe guard for when the array is empty.
   if (arr.length == 0) return [];
 
+  // Create array to hold pairs and sort the array to minimize loops
   const pairs: number[][] = [];
   arr.sort((a, b) => a - b);
 
   for (let i = 0; i < arr.length - 1; i++) {
+    // If Item is lesser than target, we do not need to perform any further
+    // computation as it can not be a member of the pairs array.
     if (arr[i] > target) continue;
 
     for (let j = i + 1; j < arr.length; j++) {
+      // When a pair is found, we validate that it is not already in the pairs
+      // array by utilizing the helper compareArray() function.
       if (arr[i] + arr[j] == target) {
         const newPair: number[] = [arr[i], arr[j]];
         let present = false;
 
+        // Compare current pair with all pairs
         for (let k = 0; k < pairs.length; k++) {
           if (compareArray(pairs[k], newPair)) present = true;
         }
